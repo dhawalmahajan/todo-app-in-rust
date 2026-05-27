3744bb8911173f357888374f10cee49c149c8aba
Dhawal Mahajan <dhawal.mahajan1@gmail.com>
2026-05-27 13:02:10 +0530

    Add simple match logic for console messages

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::io::stdin;
+
 fn main() {
-    println!("Hello, world!");
+    println!("------- RUST Todo Applications ------- ");
+    println!("Press any keys between 1 to 4 to perform action: ");
+    println!("1. View All Todos");
+    println!("2. Add a Todo");
+    println!("3. Edit a todo");
+    println!("4. Exit");
+    let mut input = String::new();
+    stdin().read_line(&mut input).expect("Failed to read line");
+    let mut user_input = input.trim().parse();
+    match user_input {
+        Ok(1) => println!(" View All Todos"),
+        Ok(2) => println!("Add a Todo"),
+        Ok(3) => println!("  Edit a todo"),
+        Ok(4) => println!(" Exit"),
+        _ => println!("Invalid Output"),
+    };
 }
