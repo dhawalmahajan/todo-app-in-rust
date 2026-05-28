550395ca561f390c5564b065190e0225297091b4
Dhawal Mahajan <dhawal.mahajan1@gmail.com>
2026-05-28 14:55:11 +0530

    Add editing method and addition of todo method

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,24 @@
-use core::task;
-use std::io::stdin;
+use std::io::{Write, stdin, stdout};
 #[derive(Debug)]
 struct Task {
     task: String,
-    is_Completed: bool,
+    is_completed: bool,
 }
 fn main() {
     let mut tasks: Vec<Task> = Vec::new();
-    main_menu();
-    let mut input = String::new();
-    stdin().read_line(&mut input).expect("Failed to read line");
-    let mut user_input = input.trim().parse();
-    match user_input {
-        Ok(1) => view_todos(&tasks),
-        Ok(2) => println!("Add a Todo"),
-        Ok(3) => println!("  Edit a todo"),
-        Ok(4) => println!(" Exit"),
-        _ => println!("Invalid Output"),
-    };
+    loop {
+        main_menu();
+        let mut input = String::new();
+        stdin().read_line(&mut input).expect("Failed to read line");
+        let user_input = input.trim().parse();
+        match user_input {
+            Ok(1) => view_todos(&tasks),
+            Ok(2) => add_todo(&mut tasks),
+            Ok(3) => edit_todo(&mut tasks),
+            Ok(4) => break,
+            _ => println!("Invalid Output"),
+        };
+    }
 }
 fn main_menu() {
     println!("------- RUST Todo Applications ------- ");
@@ -26,18 +27,47 @@ fn main_menu() {
     println!("2. Add a Todo");
     println!("3. Edit a todo");
     println!("4. Exit");
+    println!("\n");
+    println!("> ");
+    stdout().flush().expect("Failed to flush the line");
 }
 
 fn view_todos(todos: &Vec<Task>) {
-    if todos.len() > 0 {
+    if !todos.is_empty() {
         for (index, task) in todos.iter().enumerate() {
             println!(
                 "Task {index}: {} {}",
-                if task.is_Completed { "✅" } else { " " },
+                if task.is_completed { "✅" } else { " " },
                 task.task
             );
         }
     } else {
         println!("Wohoo, There is no pending task");
+    }
+}
+fn add_todo(tasks: &mut Vec<Task>) {
+    stdout().flush().expect("Failed to flush the line");
+    let mut input = String::new();
+    stdin().read_line(&mut input).expect("Failed to read line");
+    println!(">");
+    tasks.push(Task {
+        task: input,
+        is_completed: false,
+    });
+}
+fn edit_todo(tasks: &mut Vec<Task>) {
+    stdout().flush().expect("Failed to flush the line");
+    let mut input = String::new();
+    stdin().read_line(&mut input).expect("Failed to read line");
+    println!(">");
+    match input.trim().parse::<usize>() {
+        Ok(num) => {
+            if num > tasks.len() {
+                println!("Task is absent in list, please try again!");
+                return;
+            }
+            tasks[num].is_completed = !tasks[num].is_completed;
+        }
+        Err(error) => println!("{}", error),
     }
 }
